c8fb6d4c57f666f1e6b0f31a0381e3e4b83029a4
LeStoe11 <lennart@stoepler.de>
2024-09-23 18:43:08 +0200

    Changed adapter to use Linear layers. Now compatible with rome.

--- huggingface/transcoder_adapter.py
+++ huggingface/transcoder_adapter.py
@@ -8,6 +8,13 @@ from transformers import AutoModelForCausalLM, pipeline
 from sae_training.config import LanguageModelSAERunnerConfig
 
 
+def anthropic_style_weight_init(layer: nn.Linear):
+    layer.weight.data = nn.init.kaiming_uniform_(
+        torch.empty(*layer.weight.data.shape, dtype=layer.weight.data.dtype)
+    )
+    layer.bias.data = torch.zeros(*layer.bias.data.shape, dtype=layer.bias.data.dtype)
+
+
 class TranscoderAdapter(nn.Module):
     def __init__(self, cfg: LanguageModelSAERunnerConfig):
         super().__init__()
@@ -26,49 +33,32 @@ class TranscoderAdapter(nn.Module):
         if cfg.is_transcoder and cfg.d_out is not None:
             self.d_out = cfg.d_out
 
-        self.W_enc = nn.Parameter(
-            nn.init.kaiming_uniform_(
-                torch.empty(self.d_in, self.d_sae, dtype=self.dtype)
-            )
-        )
-        self.b_enc = nn.Parameter(
-            torch.zeros(self.d_sae, dtype=self.dtype)
-        )
+        self.up_proj = nn.Linear(in_features = self.d_in, out_features = self.d_sae)
 
-        self.down_proj = nn.Parameter(
-            nn.init.kaiming_uniform_(
-                torch.empty(self.d_sae, self.d_out, dtype=self.dtype)
-            )
-        )
+        # Use the same name and class as CodeLlama to stay compatible with ROME
+        self.down_proj = nn.Linear(in_features = self.d_sae, out_features = self.d_out)
+
+        anthropic_style_weight_init(self.up_proj)
+        anthropic_style_weight_init(self.down_proj)
 
         with torch.no_grad():
             # Anthropic normalize this to have unit columns
-            self.down_proj.data /= torch.norm(self.down_proj.data, dim=1, keepdim=True)
+            self.down_proj.weight.data /= torch.norm(self.down_proj.weight.data, dim=1, keepdim=True)
 
+        # Here we stick with TransformerLens nomenclature
         self.b_dec = nn.Parameter(
-            torch.zeros(self.d_in, dtype=self.dtype)
-        )
-
-        self.b_dec_out = nn.Parameter(
             torch.zeros(self.d_out, dtype=self.dtype)
         )
 
     def forward(self, x):
         sae_in = x - self.b_dec
 
-        hidden_pre = einops.einsum(
-                sae_in,
-                self.W_enc,
-                "... d_in, d_in d_sae -> ... d_sae",
-            ) + self.b_enc
+
+        hidden_pre = self.up_proj(sae_in)
 
         feature_acts = torch.nn.functional.relu(hidden_pre)
 
-        sae_out = einops.einsum(
-                feature_acts,
-                self.down_proj,
-                "... d_sae, d_sae d_out -> ... d_out",
-            ) + self.b_dec_out
+        sae_out = self.down_proj(feature_acts)
 
         return sae_out
 
@@ -78,8 +68,14 @@ class TranscoderAdapter(nn.Module):
         module = cls(cfg_and_states["cfg"])
 
         state_dict = cfg_and_states["state_dict"]
-        state_dict['down_proj'] = state_dict['W_dec']
+        state_dict['up_proj.weight'] = state_dict['W_enc'].T
+        state_dict['up_proj.bias'] = state_dict['b_enc']
+        state_dict['down_proj.weight'] = state_dict['W_dec'].T
+        state_dict['down_proj.bias'] = state_dict['b_dec_out']
+        del state_dict['W_enc']
+        del state_dict['b_enc']
         del state_dict['W_dec']
+        del state_dict['b_dec_out']
 
         module.load_state_dict(state_dict)
         return module
