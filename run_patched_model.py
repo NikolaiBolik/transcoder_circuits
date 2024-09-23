a55172ee9efa14366d7710cf48ee0b695c032c3c
minxue29031 <115342040+minxue29031@users.noreply.github.com>
2024-09-23 12:28:35 +0200

    load TC&CodeLlama model

---
+++ run_patched_model.py
@@ -0,0 +1,16 @@
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from huggingface.load_patched_model import patch_mlp_with_tc 
+
+
+model_name = "codellama/CodeLlama-7b-Instruct-hf"
+model = AutoModelForCausalLM.from_pretrained(model_name, ignore_mismatched_sizes=True)
+
+sae_weights_path = "/scratch/core/minxue/transcoder_circuits/codellama-transcoders-4ef/60segxwj/final_sparse_autoencoder_CodeLlama-7b-Instruct-hf_blocks.19.ln2.hook_normalized_16384.pt"
+sae_weights = torch.load(sae_weights_path)
+layer_index = 19
+
+
+patched_model = patch_mlp_with_tc(model, sae_weights, layer_index) 
+tok = AutoTokenizer.from_pretrained(model_name)
+print(patched_model)
