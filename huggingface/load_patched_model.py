6757ea074c565227cf1c4dbbb1be00f998e1657c
minxue29031 <115342040+minxue29031@users.noreply.github.com>
2024-09-23 12:17:19 +0200

    Update load_patched_model.py

--- huggingface/load_patched_model.py
+++ huggingface/load_patched_model.py
@@ -1,43 +1,56 @@
 import torch
+import sys
+import os
+from torch import nn
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-class MyCustomModelForCausalLM(AutoModelForCausalLM):
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-    #todo: move the new args to kwargs to keep the signature compatible with the parent class
-    @classmethod
-    def from_pretrained(cls, pretrained_model_name_or_path, layer_index, sae_weights_path, *model_args, **kwargs):
-        kwargs['ignore_mismatched_sizes'] = True
-        model = super().from_pretrained(pretrained_model_name_or_path, *model_args, **kwargs)
-
-        with torch.no_grad():
-            print("MyCustomModelForCausalLM:from_pretrained: Patching layer ", layer_index)
-
-            mlp = model.model.layers[layer_index].mlp
-
-            # Todo: get the shape of the weight matrices from the tc model instead of hardcoding
-            #tc_dim = 65536
-            tc_dim = 16384  # Artur's tc_dim
-            # Create and add the new weight matrices to the model
-            mlp.up_proj = torch.nn.Linear(in_features=4096, out_features=tc_dim, bias=False)
-            mlp.down_proj = torch.nn.Linear(in_features=tc_dim, out_features=4096, bias=False)
-            mlp.gate_proj = torch.nn.Linear(in_features=4096, out_features=tc_dim, bias=False)
-            mlp.gate_proj.weight.data.fill_(1) 
-            
-            sae_weights = torch.load(sae_weights_path)
+import sae_training
  
-            W_enc = sae_weights['state_dict']['W_enc']  
-            W_dec = sae_weights['state_dict']['W_dec']  
+def patch_mlp_with_tc(model, sae_weights, layer_index):
+    """
+    Patch the model's MLP layer at the given layer_index with new weight matrices from the SAE weights.
+    
+    Args:
+    - model: Pretrained model to be patched.
+    - sae_weights: SAE weights loaded from file.
+    - layer_index: Index of the model layer to be patched.
+    
+    Returns:
+    - Patched model.
+    """
+    with torch.no_grad():
+        print(f"Patching layer {layer_index}...")
 
-            mlp.up_proj.weight.data.copy_(W_enc.T)  
-            mlp.down_proj.weight.data.copy_(W_dec.T)  
+        mlp = model.model.layers[layer_index].mlp
 
- 
-        return model
+        W_enc = sae_weights['state_dict']['W_enc']
+        W_dec = sae_weights['state_dict']['W_dec']
+        
+        tc_dim = W_enc.shape[1]
+        input_dim = W_enc.shape[0]
 
+        print("==>>>in_features:", input_dim)
+        print("==>>>out_features:", tc_dim)
 
-### Debugging helper methods
-# Debug method to extract the activation of a specific layer
+        mlp.up_proj = nn.Linear(in_features=input_dim, out_features=tc_dim, bias=False)
+        mlp.down_proj = nn.Linear(in_features=tc_dim, out_features=input_dim, bias=False)
+        mlp.gate_proj = nn.Linear(in_features=input_dim, out_features=tc_dim, bias=False)
+
+        mlp.up_proj.weight.data.copy_(W_enc.T)
+        mlp.down_proj.weight.data.copy_(W_dec.T)
+        mlp.gate_proj.weight.data.fill_(1)
+
+        print(f"Patched MLP layer {layer_index} with tc_dim={tc_dim}")
+
+    return model
+    
+
 def load_model_and_extract_activations(model, input_text, layer_index, activation_name="up_proj"):
+    """
+    Helper function to extract the activation of a specific layer.
+    """
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     inputs = tokenizer(input_text, return_tensors="pt")
     activations = {}
@@ -59,9 +72,12 @@ def load_model_and_extract_activations(model, input_text, layer_index, activatio
     return activation
 
 
-def compare_weights(upd_model, orig_model, data_sae, layer_index):
+def compare_weights(upd_model, orig_model, sae_weights, layer_index):
+    """
+    Compare the MLP weights of the updated model with the original model and SAE weights.
+    """
     def show_mlp_weights(layer_index, model_to_show, model_name):
-        print(f"====================>>check: {model_name}<<======================")
+        print(f"====================>> {model_name} <<======================")
         print("mlp.up_proj.weight:", model_to_show.model.layers[layer_index].mlp.up_proj.weight)
         print("mlp.down_proj.weight:", model_to_show.model.layers[layer_index].mlp.down_proj.weight)
         print("mlp.gate_proj.weight:", model_to_show.model.layers[layer_index].mlp.gate_proj.weight)
@@ -69,39 +85,43 @@ def compare_weights(upd_model, orig_model, data_sae, layer_index):
         print("mlp.down_proj.weight.shape:", model_to_show.model.layers[layer_index].mlp.down_proj.weight.shape)
         print("mlp.gate_proj.weight.shape:", model_to_show.model.layers[layer_index].mlp.gate_proj.weight.shape)
 
-    show_mlp_weights(layer_index, upd_model, "updated model")
+    show_mlp_weights(layer_index, upd_model, "Updated Model")
+    show_mlp_weights(layer_index, orig_model, "Original Model")
 
-    print("====================>>check: sae weight<<======================")
-    print("W_enc.shape:", data_sae['state_dict']['W_enc'].shape)
-    print("W_dec.shape:", data_sae['state_dict']['W_dec'].shape)
-    print("W_enc:", data_sae['state_dict']['W_enc'])
-    print("W_dec:", data_sae['state_dict']['W_dec'])
-
-    show_mlp_weights(layer_index, orig_model, "original model")
+    print("====================>> SAE Weights <<======================")
+    print("W_enc.shape:", sae_weights['state_dict']['W_enc'].shape)
+    print("W_dec.shape:", sae_weights['state_dict']['W_dec'].shape)
+    print("W_enc:", sae_weights['state_dict']['W_enc'])
+    print("W_dec:", sae_weights['state_dict']['W_dec'])
 
 
 def compare_activations(upd_model, orig_model, input_text, layer_index):
-    print("====================>>check: cache activation from upd weight<<======================")
+    """
+    Compare the activations of the updated model and the original model.
+    """
+    print("====================>> Activations with Updated Weights <<======================")
     up_proj_activation_upd = load_model_and_extract_activations(upd_model, input_text, layer_index)
     print("up_proj_activation_upd:", up_proj_activation_upd)
     print("up_proj_activation_upd.shape:", up_proj_activation_upd.shape)
 
-    print("====================>>check: cache activation from ori weight<<======================")
+    print("====================>> Activations with Original Weights <<======================")
     up_proj_activation_ori = load_model_and_extract_activations(orig_model, input_text, layer_index)
     print("up_proj_activation_ori:", up_proj_activation_ori)
     print("up_proj_activation_ori.shape:", up_proj_activation_ori.shape)
 
 
+
 if __name__ == '__main__':
     model_name = "codellama/CodeLlama-7b-Instruct-hf"
-    sae_weights_path_min = "/scratch/core/minxue/transcoder-distribute/codellama-transcoders-layerlayer_index-ef16-codeparrot/d8lcir9q/final_sparse_autoencoder_CodeLlama-7b-Instruct-hf_blocks.layer_index.ln2.hook_normalized_65536.pt"
-    sae_weights_path_artur = '/nfs/data/shared/codellama-transcoders/12ok1dny/final_sparse_autoencoder_CodeLlama-7b-Instruct-hf_blocks.19.ln2.hook_normalized_16384.pt'
-    layer_index = 19
+    sae_weights_path = "/scratch/core/minxue/transcoder_circuits/codellama-transcoders-4ef/60segxwj/final_sparse_autoencoder_CodeLlama-7b-Instruct-hf_blocks.19.ln2.hook_normalized_16384.pt"
     input_text = "hello world"
+    layer_index = 19  # The index of the layer to patch
 
-    upd_model = MyCustomModelForCausalLM.from_pretrained(model_name, layer_index, sae_weights_path_artur)
     orig_model = AutoModelForCausalLM.from_pretrained(model_name)
-    data_sae = torch.load(sae_weights_path_artur)
+    model = AutoModelForCausalLM.from_pretrained(model_name, ignore_mismatched_sizes=True)
+    sae_weights = torch.load(sae_weights_path)
+    patched_model = patch_mlp_with_tc(model, sae_weights, layer_index)
 
-    compare_weights(upd_model, orig_model, data_sae, layer_index)
-    compare_activations(upd_model, orig_model, input_text, layer_index)
+    # Compare the weights and activations
+    compare_weights(patched_model, orig_model, sae_weights, layer_index)
+    compare_activations(patched_model, orig_model, input_text, layer_index)
